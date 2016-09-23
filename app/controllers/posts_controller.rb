4e851c85f2303855c8ebc5f49db3eac1db9e8cba
simposk <simposk@gmail.com>
2016-09-23 20:49:55 +0300

    Generate user model, add authentication and authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :is_owner, only: [:edit, :destroy]
 
   def index
     @posts = Post.all.order("created_at DESC")
@@ -13,7 +15,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to @post
     else
@@ -40,10 +42,16 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :user)
   end
 
   def find_post
     @post = Post.find(params[:id])
+  end
+
+  def is_owner
+    unless current_user == @post.user
+      redirect_to @post
+    end
   end
 end
