a39b3624a44d7bf0f09a8ff36572b59100e21a02
simposk <simposk@gmail.com>
2016-09-23 21:06:58 +0300

    style flashes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
-      redirect_to @post
+      redirect_to @post, notice: "Post was successfully created!"
     else
       render "new"
     end
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
 
   def update
     if @post.update(post_params)
-      redirect_to @post
+      redirect_to @post, notice: "Post was successfully updated!"
     else
       render "edit"
     end
@@ -36,6 +36,7 @@ class PostsController < ApplicationController
 
   def destroy
     @post.destroy
+    flash[:notice] = "Post was successfully deleted!"
     redirect_to root_path
   end
 
@@ -51,6 +52,7 @@ class PostsController < ApplicationController
 
   def is_owner
     unless current_user == @post.user
+      flash[:alert] = "That post doesn't belong to you!"
       redirect_to @post
     end
   end
